e6583a7a92a0a8eb4933ddb672041282c7645dcc
Бауков Кирилл <k.baukov@solarsecurity.ru>
2018-12-18 19:49:33 +0300

    Add ping functionality

--- handle/wshandler.go
+++ handle/wshandler.go
@@ -2,6 +2,7 @@ package handle
 
 import (
 	"encoding/json"
+	"github.com/jasonlvhit/gocron"
 	"homec/db"
 	"homec/ent"
 	"log"
@@ -15,10 +16,17 @@ import (
 const (
 	writeWait = 10 * time.Second 		// Time allowed to write a message to the peer.
 	maxMessageSize = 8192  				// Maximum message size allowed from peer.
+	pingWait = 60 * time.Second 		// Time allowed to read the next pong message from the peer.
 	pongWait = 60 * time.Second 		// Time allowed to read the next pong message from the peer.
 	pingPeriod = (pongWait * 9) / 10	// Send pings to peer with this period. Must be less than pongWait.
 	closeGracePeriod = 10 * time.Second	// Time to wait before force close on connection.
 )
+
+func init() {
+	log.Println("### Start Ping Scheduler ###")
+	gocron.Every(1).Minute().Do(pingActiveDevices)
+	gocron.Start()
+}
 
 func internalError(ws *websocket.Conn, msg string, err error) {
 	log.Println(msg, err)
@@ -63,26 +71,45 @@ func ServeWs(db db.DbService) http.HandlerFunc {
 		//defer ws.Close() !!!! Important
 
 		WsConnections[deviceId] = ws
+		if(ws != nil) {
+			//ws.SetPingHandler(func(string) error { ws.SetReadDeadline(time.Now().Add(pingWait)); return nil })
+			//ws.SetPongHandler(func(string) error { ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+			ws.SetPingHandler(ping)
+			ws.SetPongHandler(pong)
+			log.Println("Create new Ws Connection: succes, device: ", deviceId)
+			go wsProcessor(ws, db, deviceId)
+		} else {
+			log.Println("Ws Connection lost for device: ", deviceId)
+		}
 
-		log.Println("Create new Ws Connection: succes, device: ", deviceId)
-		go wsProcessor(ws, db)
 
 	}
 }
 
-func wsProcessor(c *websocket.Conn, db db.DbService) {
+func ping(appData string) error {
+	log.Println("[WS] Ping: "+appData)
+	return nil
+}
+
+func pong(appData string) error {
+	log.Println("[WS] Pong: "+appData)
+	return nil
+}
+
+func wsProcessor(c *websocket.Conn, db db.DbService, dId string) {
 	defer c.Close()
 	devId := getDevIdByConn(c)
 	for {
 		mt, message, err := c.ReadMessage()
 		if err != nil {
-			log.Println("[WS]:recv:", err)
+			log.Println("[WS]:error:  ", err)
+			WsConnections[dId] = nil
 			break
 		}
-		log.Printf("[WS]:recv: %s, type: %d", message, mt)
+		log.Printf("[WS]:recive: %s, type: %d", message, mt)
 		msg := string(message)
 
-		if strings.Contains(msg, "{action:connect") {
+		if strings.Contains(msg, "\"action\":\"connect\"") {
 			if !sendMsg(c, "{action:connect,success:true}") {
 				break
 			}
@@ -215,3 +242,19 @@ func getDevIdByConn(c *websocket.Conn) string {
 }
 
 
+func pingActiveDevices() {
+	log.Println("### Ping Devices ###")
+	for devId, c := range WsConnections {
+		if c != nil {
+			if !sendMsg(c, "ping") {
+				log.Println("Send ping to " + devId + ": failed")
+				WsConnections[devId] = nil
+			} else {
+				log.Println("Send ping to " + devId )
+			}
+		} else {
+			log.Println("Device " + devId + "is die" )
+		}
+
+	}
+}
