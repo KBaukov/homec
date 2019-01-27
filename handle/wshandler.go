4fcd81f73967f0521a0f17688acb74144ae6d839
unknown <kbaukov@gmail.com>
2019-01-27 11:20:35 +0300

    ping scheduler on

--- handle/wshandler.go
+++ handle/wshandler.go
@@ -27,7 +27,7 @@ const (
 
 func init() {
 	//log.Println("### Start Ping Scheduler ###")
-	gocron.Every(10).Second ().Do(pingActiveDevices)
+	gocron.Every(1).Minute().Do(pingActiveDevices)
 	gocron.Start()
 }
 
